c9cdae94d9f39f7c5666535bcda7dc5d040c5569
Tainá Peixoto <tsouzapeixoto@gmail.com>
2024-09-25 21:48:22 -0300

    feat: Inclusão do pacote aluguel

---
+++ src/model/aluguel/Devolucao.java
@@ -0,0 +1,11 @@
+package model.aluguel;
+
+import java.time.LocalDateTime;
+
+public class Devolucao {
+
+    private Aluguel aluguel;
+    private LocalDateTime dataDevolucaoFinal;
+    protected Boolean devolvido;
+
+}
